a8ce387f10ab7bd2ca14ecc7cfbbe852a23a7ccc
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-16 14:09:33 -0700

    added form statically, got form from the dom, added event listener, created a new formdata upon event listener, consolelog to make sure inputs from form work properly

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log(`hello from app.js`);
+// console.log(`hello from app.js`);
+
+
+// get the form from the DOM 
+const myForm = document.getElementById('user-form'); 
+
+
+
+// event listener 
+myForm.addEventListener('submit', (event) => {
+    // set the event to prevent default
+    event.preventDefault(); 
+
+    const form = new FormData(myForm); // now we have a form 
+
+    // console.log(form.get('class'), form.get('name')); // when inputing something in the form at this point, it will console log your user input
+
+}); 
